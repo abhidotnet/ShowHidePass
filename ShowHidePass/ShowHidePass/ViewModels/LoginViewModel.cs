86e6f3731e8d1f294f3d568752c9fbf06f5b50ad
abhijit <abhijit.shrikhande@gmail.com>
2021-11-29 13:32:25 -0500

    Changed to fix location and also the code. Now working as per design.

--- ShowHidePass/ShowHidePass/ViewModels/LoginViewModel.cs
+++ ShowHidePass/ShowHidePass/ViewModels/LoginViewModel.cs
@@ -1,10 +1,6 @@
-﻿using System.Windows.Input;
-using ShowHidePass.Views;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ShowHidePass.Views;
+using System.Windows.Input;
 using Xamarin.Forms;
-using Android.Content.Res;
 
 namespace ShowHidePass.ViewModels
 {
@@ -13,21 +9,28 @@ namespace ShowHidePass.ViewModels
 
         public string UserName { get; set; }
         public string Password { get; set; }
-        public bool HidePassword { get; set; }
+        bool hidePassword;
+        public bool HidePassword { 
+            get { return hidePassword; }
+            set {
+                hidePassword = value;
+                OnPropertyChanged(nameof(HidePassword));
+            } 
+        }
+        string eyeIcon = "eyehidden.png";
         public string EyeIcon
         {
             get
             {
-                return HidePassword ? "eye.png" : "eyehidden.png";
+                return eyeIcon;
             }
             set
             {
-                EyeIcon = value;
+                eyeIcon = value;
                 OnPropertyChanged(nameof(EyeIcon));
             }
         }
-        public ICommand ShowPasswordCommand =>
-            new Command(HidePasswordChange);
+        public ICommand ShowPasswordCommand { get; }
         public Command LoginCommand { get; }
 
         public LoginViewModel()
@@ -35,11 +38,21 @@ namespace ShowHidePass.ViewModels
             
             HidePassword = true;
             LoginCommand = new Command(OnLoginClicked);
+
+            ShowPasswordCommand = new Command(HidePasswordChange);
         }
 
         private void HidePasswordChange()
         {
             HidePassword = !HidePassword;
+            if (HidePassword)
+            {
+                EyeIcon = "eyehidden.png";
+            }
+            else
+            {
+                EyeIcon = "eye.png";
+            }
         }
 
         private async void OnLoginClicked(object obj)
